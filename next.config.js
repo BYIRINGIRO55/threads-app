b3174d3417509f2f7fee786224903aea3ff65e72
BYIRINGIRO55 <byiringirosamuel533@gmail.com>
2025-05-06 16:08:42 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io", // 🔥 Add this line
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
